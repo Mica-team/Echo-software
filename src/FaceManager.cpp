96a50b53f98d7a5e14f39aef9f674f1cb5007f56
jimj90201-a11y <jimj90201@gmail.com>
2026-07-26 19:28:24 +0530

    Update FaceManager.cpp

--- src/FaceManager.cpp
+++ src/FaceManager.cpp
@@ -7,9 +7,9 @@
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 
-#define OLED_DC   2
-#define OLED_RST  4
-#define OLED_CS   -1
+#define OLED_DC 2
+#define OLED_RST 4
+#define OLED_CS -1
 
 Adafruit_SSD1306 display(
   SCREEN_WIDTH,
@@ -20,35 +20,51 @@ Adafruit_SSD1306 display(
   OLED_CS
 );
 
-void FaceManager::begin() {
 
-  Serial.begin(115200);
+void faceSetup()
+{
+    SPI.begin(18, -1, 23, -1);
 
-  SPI.begin(18, -1, 23, -1);
-
-  if (!display.begin(SSD1306_SWITCHCAPVCC)) {
-    Serial.println("OLED FAILED");
-    while (true) {
-      delay(1000);
+    if (!display.begin(SSD1306_SWITCHCAPVCC)) {
+        Serial.println("OLED FAILED");
+        while(true);
     }
-  }
 
-  display.clearDisplay();
-
-  // Left eye
-  display.fillCircle(40, 24, 6, SSD1306_WHITE);
-
-  // Right eye
-  display.fillCircle(88, 24, 6, SSD1306_WHITE);
-
-  // Smile
-  display.drawCircle(64, 40, 16, SSD1306_WHITE);
-  display.fillRect(48, 24, 32, 16, SSD1306_BLACK);
-
-  display.display();
+    idleFace();
 }
 
 
-void FaceManager::update() {
-  // Future animations go here
+void happyFace()
+{
+    display.clearDisplay();
+
+    display.fillCircle(40, 24, 6, SSD1306_WHITE);
+    display.fillCircle(88, 24, 6, SSD1306_WHITE);
+
+    display.drawCircle(64, 40, 16, SSD1306_WHITE);
+    display.fillRect(48, 24, 32, 16, SSD1306_BLACK);
+
+    display.display();
+}
+
+
+void idleFace()
+{
+    display.clearDisplay();
+
+    display.fillCircle(40, 24, 6, SSD1306_WHITE);
+    display.fillCircle(88, 24, 6, SSD1306_WHITE);
+
+    display.display();
+}
+
+
+void sleepFace()
+{
+    display.clearDisplay();
+
+    display.drawLine(35,24,45,24,SSD1306_WHITE);
+    display.drawLine(83,24,93,24,SSD1306_WHITE);
+
+    display.display();
 }
