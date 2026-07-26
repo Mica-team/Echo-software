9e3d23a30bfb6394ad6786baa55bbce9a811d612
jimj90201-a11y <jimj90201@gmail.com>
2026-07-26 18:44:36 +0530

    Delete ota directory

--- ota/Config.h
+++
@@ -1,9 +0,0 @@
-#define WIFI_SSID "YOUR WIFI"
-#define WIFI_PASSWORD "YOUR PASSWORD"
-
-#define GITHUB_USER "Gamerofpro"
-#define GITHUB_REPO "Echo"
-
-#define VERSION_URL "https://raw.githubusercontent.com/Gamerofpro/Echo/main/version.json"
-
-#define FIRMWARE_URL "https://github.com/Gamerofpro/Echo/releases/latest/download/firmware.bin"
