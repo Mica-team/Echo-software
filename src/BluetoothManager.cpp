a39065c03c897a12b50c815969567421b2d167ee
jimj90201-a11y <jimj90201@gmail.com>
2026-07-26 22:18:21 +0530

    Update BluetoothManager.cpp

--- src/BluetoothManager.cpp
+++ src/BluetoothManager.cpp
@@ -4,19 +4,26 @@
 
 BluetoothSerial SerialBT;
 
-String command="";
+String command = "";
 
 void bluetoothSetup()
 {
-    SerialBT.begin("Echo");
-    Serial.println("Bluetooth Ready");
+    delay(2000);
+
+    if (!SerialBT.begin("Echo")) {
+        Serial.println("Bluetooth FAILED!");
+        while (true) {
+            delay(1000);
+        }
+    }
+
+    Serial.println("Bluetooth READY");
 }
 
 void bluetoothLoop()
 {
-    if(SerialBT.available())
-    {
-        command=SerialBT.readStringUntil('\n');
+    if (SerialBT.available()) {
+        command = SerialBT.readStringUntil('\n');
         command.trim();
         Serial.println(command);
     }
