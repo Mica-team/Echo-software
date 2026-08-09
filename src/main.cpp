d8ee8649641d4e6215bb64fd7f2578d6b239206d
jimj90201-a11y <jimj90201@gmail.com>
2026-08-09 14:59:58 +0530

    Revert onboard LED changes and restore normal main loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,19 @@
 #include <Arduino.h>
-#include <Preferences.h>
 
 #include "BluetoothManager.h"
 #include "ServoManager.h"
 #include "FaceManager.h"
 #include "OTAManager.h"
 
-// ESP32 DevKit onboard blue LED is normally connected to GPIO 2.
-#ifndef ECHO_BLUE_LED_PIN
-#define ECHO_BLUE_LED_PIN 2
-#endif
-
-// OTAManager stores this value before rebooting after a successful update.
-// false = LED OFF, true = LED ON. Every successful OTA flips the state.
-static Preferences ledPreferences;
-
 void setup()
 {
     Serial.begin(115200);
 
-    pinMode(ECHO_BLUE_LED_PIN, OUTPUT);
-
-    // Remember the LED state selected by the previous successful OTA.
-    // First boot defaults to OFF.
-    bool ledOn = false;
-    ledPreferences.begin("echo-led", true);
-    ledOn = ledPreferences.getBool("ota_led", false);
-    ledPreferences.end();
-
-    digitalWrite(ECHO_BLUE_LED_PIN, ledOn ? HIGH : LOW);
-
-    delay(500);
-
-    Serial.println();
-    Serial.println("====================");
-    Serial.println("      ECHO BOOT     ");
-    Serial.println("====================");
-    Serial.printf("OTA LED state: %s\n", ledOn ? "ON" : "OFF");
-
     bluetoothSetup();
     delay(500);
-
     servoSetup();
-    delay(100);
-
     faceSetup();
-    delay(100);
-
     otaSetup();
-
-    Serial.println("Echo ready.");
 }
 
 void loop()
@@ -87,12 +51,6 @@ void loop()
         sleepFace();
         command = "";
     }
-    else if (command.length() > 0)
-    {
-        Serial.print("Unknown command: ");
-        Serial.println(command);
-        command = "";
-    }
 
-    delay(5);
+    delay(20);
 }
