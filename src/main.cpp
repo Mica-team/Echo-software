2a9fa8827755220713ea988ebacb30870e31edb5
jimj90201-a11y <jimj90201@gmail.com>
2026-08-09 10:23:32 +0530

    Improve Echo startup and Bluetooth loop responsiveness

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,25 @@ void setup()
 {
     Serial.begin(115200);
 
+    delay(500);
+
+    Serial.println();
+    Serial.println("====================");
+    Serial.println("      ECHO BOOT     ");
+    Serial.println("====================");
+
     bluetoothSetup();
     delay(500);
 
     servoSetup();
+    delay(100);
+
     faceSetup();
+    delay(100);
+
     otaSetup();
+
+    Serial.println("Echo ready.");
 }
 
 void loop()
@@ -22,49 +35,42 @@ void loop()
     bluetoothLoop();
     otaLoop();
 
-    // -----------------------------
-    // Servo commands
-    // -----------------------------
-
     if (command == "LEFT")
     {
         servoLeft();
         command = "";
     }
-
     else if (command == "RIGHT")
     {
         servoRight();
         command = "";
     }
-
     else if (command == "CENTER")
     {
         servoCenter();
         command = "";
     }
-
-    // -----------------------------
-    // Face commands
-    // -----------------------------
-
     else if (command == "HAPPY")
     {
         happyFace();
         command = "";
     }
-
     else if (command == "IDLE")
     {
         idleFace();
         command = "";
     }
-
     else if (command == "SLEEP")
     {
         sleepFace();
         command = "";
     }
+    else if (command.length() > 0)
+    {
+        Serial.print("Unknown command: ");
+        Serial.println(command);
+        command = "";
+    }
 
-    delay(20);
+    delay(5);
 }
